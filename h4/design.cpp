493f64a39697c4e0c9d1d6035bc1ceacbce93313
edward88421 <36149404+edward88421@users.noreply.github.com>
2022-05-02 00:07:42 +0800

    Add files via upload

---
+++ h4/design.cpp
@@ -0,0 +1,16 @@
+module BCD(clk, rst_asyn, Q_out);
+input clk;
+input rst_asyn;
+output [3:0] Q_out;
+reg [3:0] Q_out=0;
+  
+always@ (posedge clk or posedge rst_asyn)
+begin
+  if (rst_asyn)
+ 	Q_out=0;
+  else if (Q_out == 9)
+	Q_out=0;
+  else
+	Q_out++;
+end
+endmodule
